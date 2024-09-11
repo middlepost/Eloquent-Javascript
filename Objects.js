1443911c47a60de5180091ae9c607f1c2137dae6
middlepost <88712713+middlepost@users.noreply.github.com>
2024-09-11 14:44:16 +0930

    Update to object.js

--- Objects.js
+++ Objects.js
@@ -30,36 +30,32 @@ console.log(new Vec(1, 2).minus(new Vec(2, 3)));
 console.log(new Vec(3, 4).length);
 
 // Group
+// Lesson learnt here is that the "in" keyword checks if the index of a value exists in js 
+//and not if the value itself exists
 class Group {
     constructor(){
         this.member = []
     }
     add(value){
-        if (!(value in this.member)){
+        if (!this.member.includes(value)){
             this.member.push(value)
         }
     }
     delete(value){
-        if(value in this.member){
-            this.member.pop(value)
-        }
+        this.member = this.member.filter(m => m !== value)
     }
     has(value){
-        if (value in this.member){
-            return true
-        }
-        else
-            return false
+        return this.member.includes(value)
     }
 
     static from(arrayit){
-        let group = new Group()
-        for(let x = 0; x <= arrayit.length-1; x++){
-            group.add(arrayit[x])
+        let hh = new Group()
+        for(let x of arrayit){
+            hh.add(x)
         }
-
-        }
+        return hh
     }
+}
 
 let group = Group.from([10, 20]);
 console.log(group.has(10));
@@ -69,7 +65,50 @@ console.log(group.has(10));
 
 
 
+// Iterable Group class
 
+class GroupIT {
+    constructor(){
+        this.member = []
+    }
+    [Symbol.iterator](){
+        let index = 0
+        let arr = this.member
+        return {
+            next() {
+                if(index < arr.length){
+                    return {value: arr[index++], done: false }
+                }
+                else
+                    return{value: undefined, done: true}
+            }
+
+        }
+    }
+    add(value){
+        if (!this.member.includes(value)){
+            this.member.push(value)
+        }
+    }
+    delete(value){
+        this.member = this.member.filter(m => m !== value)
+    }
+    has(value){
+        return this.member.includes(value)
+    }
+
+    static from(arrayit){
+        let hh = new GroupIT()
+        for(let x of arrayit){
+            hh.add(x)
+        }
+        return hh
+    }
+}
+
+for (let value of GroupIT.from(["a", "b", "c"])) {
+    console.log(value);
+  }
 
 
 
